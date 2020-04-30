37c83b54c7f943c1d331648161d3e3099cccfc8f
mike1789 <63952245+mike1789@users.noreply.github.com>
2020-04-30 18:48:42 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -19,6 +19,8 @@ root.title(program_name)
 #section title
 tk.Label(root, text = "Limit Order", font = ("arial", 10, 'bold')).grid(row = 0, column = 3)
 tk.Label(root, text = "Market Order", font = ("arial", 10, 'bold')).grid(row = 7, column = 3)
+tk.Label(root, text = "Open Positions", foreground = "green", font = ('arial', 10 , 'bold',)).grid(row = 12, column = 1)
+tk.Label(root, text = "Open Orders", foreground = "green", font = ('arial', 10 , 'bold',)).grid(row = 14, column = 1)
 # labels for main selling/buying windows
 
 enterbuy_qty = tk.Entry(root)
@@ -70,6 +72,10 @@ buybut = tk.Button(root, text = "Buy/Long")
 buybut.grid(row = 5, column =2 )
 sellbut = tk.Button(root, text = "Sell/Short")
 sellbut.grid(row = 5, column =4 )
+buybut2 = tk.Button(root, text = "Buy/Long")
+buybut2.grid(row = 11, column =2 )
+sellbut2 = tk.Button(root, text = "Sell/Short")
+sellbut2.grid(row = 11, column =4 )
 
 #Market buy/sells
 mktqtybuy = tk.Label(root, text = "Qty")
@@ -86,11 +92,33 @@ mktselltotal = tk.Label(root, text = "Total")
 mktselltotal.grid(row =10, column =3)
 
 #display BTC price
-current_price_label = tk.Label(root, text = "BTC/USDT Current Price:",font = ("arial", 10, 'bold'))
-current_price_label.grid(row = 11, column = 2)
-current_price = tk.Label(root, text = btc, font = ("arial", 10, 'bold'))
-current_price.grid(row = 11, column = 3)
+# current_price_label = tk.Label(root, text = "BTC/USDT Current Price:",font = ("arial", 10, 'bold'))
+# current_price_label.grid(row = 11, column = 2)
+# current_price = tk.Label(root, text = btc, font = ("arial", 10, 'bold'))
+# current_price.grid(row = 11, column = 3)
+
+#open orders section
+open_positions = tk.Label(root, text = "Symbol").grid(row = 13, column = 1)
+open_positions = tk.Label(root, text = "Size").grid(row = 13, column = 2)
+open_positions = tk.Label(root, text = "Entry").grid(row = 13, column = 3)
+open_positions = tk.Label(root, text = "Liqu. Price").grid(row = 13, column = 4)
+open_positions = tk.Label(root, text = "Market price").grid(row = 13, column = 5)
+open_positions = tk.Label(root, text = "PNL",width = 15).grid(row = 13, column = 6)
+open_positions = tk.Label(root, text = "RPNL",width = 15).grid(row = 13, column = 7)
+open_positions = tk.Label(root, text = "Margin",width = 15).grid(row = 13, column = 8)
+open_positions = tk.Label(root, text = "Actions",width = 15).grid(row = 13, column = 9)
+
+#Completed orders section
+open_orders = tk.Label(root, text = "Time").grid(row = 20, column = 1)
+open_orders = tk.Label(root, text = "Symbol").grid(row = 20, column = 2)
+open_orders = tk.Label(root, text = "Order Type").grid(row = 20, column = 3)
+open_orders = tk.Label(root, text = "Side").grid(row = 20, column = 4)
+open_orders = tk.Label(root, text = "Price").grid(row = 20, column = 5)
+open_orders = tk.Label(root, text = "Filled",width = 15).grid(row = 20, column = 6)
+open_orders = tk.Label(root, text = "Order Amount",width = 15).grid(row = 20, column = 7)
+open_orders = tk.Label(root, text = "		",width = 15).grid(row = 20, column = 8)
+open_orders = tk.Label(root, text = "		",width = 15).grid(row = 20, column = 9)
 
 
-
+root.mainloop()
 root.mainloop()
